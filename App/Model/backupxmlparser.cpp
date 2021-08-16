6e59857942a3118bd178862fedf9c62e7cc6cd91
benyamin saedi <benyamin.saedi@gmail.com>
2021-08-16 12:59:53 +0430

    initialized

---
+++ App/Model/backupxmlparser.cpp
@@ -0,0 +1,6 @@
+#include "backupxmlparser.h"
+
+BackupXMLParser::BackupXMLParser()
+{
+
+}
---
+++ App/Model/backupxmlparser.h
@@ -0,0 +1,12 @@
+#ifndef BACKUPXMLPARSER_H
+#define BACKUPXMLPARSER_H
+
+#include <QObject>
+
+class BackupXMLParser
+{
+public:
+    BackupXMLParser();
+};
+
+#endif // BACKUPXMLPARSER_H
